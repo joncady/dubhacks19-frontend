a394878f95a6b01a83c8fa9e3e1aee1ad1e60ebc
Jonathan Cady <joncady@uw.edu>
2019-10-13 08:36:38 -0700

    add prod url

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const PROD_URL = "http://test.com";
-export const LOCAL_URL = "http://localhost:3030";
+export const PROD_URL = "https://us-central1-dubhacks19-7f555.cloudfunctions.net/widgets/meals";
+export const LOCAL_URL = "http://localhost:5000/dubhacks19-7f555/us-central1/widgets";
 export const PROD = "production";
