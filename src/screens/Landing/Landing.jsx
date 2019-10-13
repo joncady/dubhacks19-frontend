dead744659deecba893e4ea07ad23092c0f3f592
Jonathan Cady <joncady@uw.edu>
2019-10-13 11:03:38 -0700

    name change

--- src/screens/Landing/Landing.jsx
+++ src/screens/Landing/Landing.jsx
@@ -7,7 +7,7 @@ const Landing = () => {
   return (
     <div>
       <StyledLanding>
-        <StyledH1>Hapi</StyledH1>
+        <StyledH1>Snack Saver</StyledH1>
         <StyledH2>Build your smart diet.</StyledH2>
         <Button color="success" size="lg" block>Sign In</Button>{' '}
         <Link to="/signupName"><Button color="primary" size="lg" block>Sign Up</Button>{' '}</Link>
